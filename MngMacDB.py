ebec0a56c3131e0ffc0f3e2e13032f1c7c9e89be
stitchmeup <stitchmeup@users.noreply.github.com>
2021-03-19 23:54:31 +0100

    Update

--- MngMacDB.py
+++ MngMacDB.py
@@ -5,6 +5,7 @@
 
 import sqlite3
 from GenMacAddr import GenMacAddr
+import re
 
 
 class MngMacDB(sqlite3.Connection):
@@ -14,12 +15,15 @@ class MngMacDB(sqlite3.Connection):
     """
 
     def __init__(self, database):
-        sqlite3.Connection.__init__(self, database)
+        super().__init__(database)
         self._database = database
         self._cur = self.cursor()
         # % Matches any in SQL WHERE condition
         self._mac = '%'
         self._hostname = '%'
+        self._patternModes = {}
+        self._patternModes["mac"] = True
+        self._patternModes["hostname"] = True
         self._tables = []
         self.set_Tables()
 
@@ -35,34 +39,76 @@ class MngMacDB(sqlite3.Connection):
     def get_hostname(self):
         return self._hostname
 
+    def get_patternModes(self):
+        return self._patternModes
+
     def get_tables(self):
         return self._tables
 
     def set_database(self, database):
         " Connect to a new database and set a new cursor "
         self.close()
-        sqlite3.Connection.__init__(self, database)
+        super().__init__(database)
         self._database = database
         self._cur = self.cursor()
 
-    def set_mac(self, mac):
-        " Set mac from a GenMacAddr instance or if None provide set it to '%' "
-        if mac is None:
-            self._mac = '%'
-        elif isinstance(mac, GenMacAddr):
-            if not mac.isEmpty():
-                self._mac = mac.toString()
+    def set_mac(self, mac, patternMode=False):
+        """
+        Set mac from a GenMacAddr instance
+        if None:
+            set it to '%' (turn pattern mode ON)
+        if pattern mode is set:
+            doesnt check for integrity and only select is allowed
+        """
+        self._patternModes["mac"] = patternMode
+        if patternMode:
+            self._mac = mac
+        else:
+            if mac is None:
+                self._mac = '%'
+                self._patternModes["mac"] = False
+            elif isinstance(mac, GenMacAddr):
+                if not mac.isEmpty():
+                    self._mac = mac.toString()
+                else:
+                    raise ValueError(mac, "mac address is empty.")
             else:
-                raise ValueError(mac, "mac address is empty")
-        else:
-            raise TypeError(mac, "mac address is not an instance of GenMacAddr")
+                raise TypeError(mac, " is not an instance of GenMacAddr.")
 
-    def set_hostname(self, hostname):
-        " Set hostname, if provided hostname is None, set it to '%' "
-        if hostname is None:
-            self._hostname = '%'
-        else:
+    def set_hostname(self, hostname, patternMode=False):
+        """
+        Set hostname from a valid hostname
+        if None:
+            set it to '%' (turn pattern mode ON)
+        if pattern mode is set:
+            doesnt check for integrity and only select is allowed
+        """
+        self._patternModes["hostname"] = patternMode
+        if patternMode:
             self._hostname = hostname
+        else:
+            if hostname is None:
+                self._hostname = '%'
+                self._patternModes["hostname"] = True
+            else:
+                if self.__class__.isValidHostname(hostname):
+                    self._hostname = hostname
+                else:
+                    raise ValueError(hostname + " is not valid.")
+
+    def set_patternModes(self, patternModes):
+        """
+        Set patterns mode for mac and hostname
+        modes is an dictionnary with at least 1 property: mac or hostname
+        """
+        for key in patternModes:
+            if key in self._patternModes:
+                self._patternModes[key] = patternModes[key]
+            else:
+                raise ValueError(
+                    key,
+                    " invalid key. Only 'mac' or 'hostname' allowed."
+                )
 
     def set_Tables(self):
         " Set _tables to a list of existing tables of in database "
@@ -78,29 +124,126 @@ class MngMacDB(sqlite3.Connection):
         " Return True if table is in database "
         return True if table in self._tables else False
 
+    # Overrides sqlite3.Connection
+    def execute(self, sql, parameter, table):
+        if not self.tableExist(table):
+            raise ValueError(table, " does not exist in database.")
+        print(parameter)
+        self._cur.execute(sql, parameter)
+        return self._cur.fetchall()
+
     # SELECT
     def select(self, table):
-        if self.tableExist(table):
-            query = 'SELECT * FROM {}'.format(table) + ' WHERE mac LIKE :mac AND hostname LIKE :hostname;'
-            self._cur.execute(query, { "mac": self._mac, "hostname": self._hostname })
-            return self._cur.fetchall()
-        else:
-            raise ValueError(table, " does not exist in databse.")
+        query = 'SELECT * FROM {}'.format(table) + \
+         ' WHERE mac LIKE :mac AND hostname LIKE :hostname;'
+        return self.execute(query, {
+            "mac": self._mac,
+            "hostname": self._hostname
+        }, table)
+
+    # INSERT INTO
+    def insert(self, table):
+        # Prevent insert with pattern (data corruption)
+        for key in self._patternModes:
+            if self._patternModes[key]:
+                raise ValueError(
+                    "Invalid request. mac or(and) hostname is(are) pattern(s)."
+                )
+
+        query = 'INSERT INTO {}'.format(table) + \
+            '(mac, hostname) VALUES(:mac, :hostname);'
+        return self.execute(query, {
+            "mac": self._mac,
+            "hostname": self._hostname
+        }, table)
+
+    # Update abstract
+    def update(self, table, columns):
+        """ Columns is an object:
+        {
+            updated: {"colname1": value},
+            matched: {"colname2": value}
+        }
+        """
+        # Prevent update with pattern (data corruption)
+        for key in self._patternModes:
+            if self._patternModes[key]:
+                raise ValueError(
+                    "Invalid request. mac or(and) hostname is(are) pattern(s)."
+                )
+
+        query = 'UPDATE {}'.format(table) + \
+            'SET :updatedCol = :updatedVal' + \
+            'WHERE :matchedCol LIKE :matchedVal'
+        return self.execute(query, {
+            "updatedCol": columns.updated.keys()[0],
+            "updatedVal": columns.updated.values()[0],
+            "matchedCol": columns.matched.keys()[0],
+            "matchedVal": columns.matched.values()[0]
+        }, table)
+
+    def updateMac(self, table):
+        return self.update(table, {
+            "updated": {"mac": self._mac},
+            "matched": {"hostname": self._hostname},
+        })
+
+    def updateHostname(self, table):
+        return self.update(table, {
+            "updated": {"hostname": self._hostname},
+            "matched": {"mac": self._mac}
+        })
+
+    @staticmethod
+    def isValidHostname(hostname):
+        if len(hostname) > 255:
+            return False
+        if hostname[-1] == ".":
+            # strip exactly one dot from the right, if present
+            hostname = hostname[:-1]
+        allowed = re.compile("(?!-)[A-Z\d-]{1,63}(?<!-)$", re.IGNORECASE)
+        return all(allowed.match(x) for x in hostname.split("."))
+
 
 if __name__ == '__main__':
     mngMacDB = MngMacDB('mac.db')
-    # ['vbox', 'any']
     print(mngMacDB.get_tables())
-    # false
-    print(mngMacDB.tableExist('ay'))
+    # ['vbox', 'any']
+
+    print(mngMacDB.tableExist('any'))
+    # True
+
+    print(mngMacDB.select('any'))
     # dump table any
-    print(mngMacDB.select('any'))
+
+    mngMacDB.set_database('mac.db')
+    print(mngMacDB.get_database())
+    # mac.db
+
     macAddr = GenMacAddr(None)
-    macAddr.set_addr({"vendorId": [0x32, 0x5D, 0xB9], "serialId": [0x49, 0x1B, 0x5E]})
+    macAddr.set_addr({
+        "vendorId": [0x32, 0x5D, 0xB9],
+        "serialId": [0x49, 0x1B, 0x5E]
+    })
     mngMacDB.set_mac(macAddr)
+    print(mngMacDB.select('any'))
     # [('325DB9491B5E', 'host1')]
-    print(mngMacDB.select('any'))
+
     mngMacDB.set_mac(None)
-    mngMacDB.set_hostname('host 2')
-    # [('2A523B85189F', 'host 2'), ('86A1EC0CB254', 'host 2')]
+    mngMacDB.set_hostname('host-2')
     print(mngMacDB.select('any'))
+    # [('2A523B85189F', 'host 2'), ('86A1EC0CB254', 'host 2')]
+
+    mngMacDB.set_hostname('host%', True)
+    print(mngMacDB.select('any'))
+    # [('325DB9491B5E', 'host1'), ('2A523B85189F', 'host-2'), ('86A1EC0CB254', 'host-2')]
+
+
+    try:
+        mngMacDB.insert('any')
+    except ValueError as e:
+        print(e)
+    # Invalid request. mac or(and) hostname is(are) pattern(s).
+
+    print(mngMacDB.select('any'))
+    # [('325DB9491B5E', 'host1'), ('2A523B85189F', 'host-2'), ('86A1EC0CB254', 'host-2')]
--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 Manage and Generate unique LAA and VirtualBox Mac Address
 
 Still in developpement
+
+ChangeLog:
+# 19/03/21
+MngMacDB:
+- Implements SELECT, INSERT INTO and UPDATE SQLite request
+- Prevent data corruption with INSERT INTO and UPDATE (via pattern enabling modes)
+- method isValidHostname
